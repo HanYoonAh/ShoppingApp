1ec89f4f5dfb16c9bf102c16a24bd49f177bf4da
HanYoonAh <geethukrishna3718@gmail.com>
2024-05-20 00:41:37 +0530

    Global colors has been set.

--- shopping-app/src/App.jsx
+++ shopping-app/src/App.jsx
@@ -5,15 +5,17 @@ import Contact from "./components/contact/Contact";
 import About from "./components/about/About";
 import Cart from "./components/cart/Cart";
 import WishList from "./components/wishlist/WishList";
+import NavBar from "./components/common/navbar/NavBar";
 
 function App() {
   return (
     <>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/about" element={<About />} />
-        <Route path="/element" element={<Cart />} />
+        <Route path="/cart" element={<Cart />} />
         <Route path="/wishlist" element={<WishList />} />
       </Routes>
     </>
--- shopping-app/src/components/common/navbar/NavBar.jsx
+++ shopping-app/src/components/common/navbar/NavBar.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
-
+import { Link } from "react-router-dom";
+import styles from "./navbar.module.css";
 function NavBar() {
   return (
     <div>
-      
+      <nav className={styles.nav_main_container}>
+        <p className={styles.main_logo}>GeethSpace</p>
+        <ul className={styles.nav_container}>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="/cart">Cart</Link>
+          </li>
+          <li>
+            <Link to="/wishlist">WishList</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
