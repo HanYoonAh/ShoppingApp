c2adec8419af49acc68e5b6bc7e80e6e86f85566
HanYoonAh <geethukrishna3718@gmail.com>
2024-05-20 16:49:06 +0530

    Main navbar and Global color theme has been set.

--- shopping-app/src/components/common/navbar/NavBar.jsx
+++ shopping-app/src/components/common/navbar/NavBar.jsx
@@ -4,11 +4,11 @@ function NavBar() {
   return (
     <div>
       <nav className={styles.nav_main_container}>
-        <p className={styles.main_logo}>GeethSpace</p>
+        <Link to="/">
+          <p className={styles.main_logo}>GeethSpace</p>
+        </Link>
         <ul className={styles.nav_container}>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
+
           <li>
             <Link to="/about">About</Link>
           </li>
